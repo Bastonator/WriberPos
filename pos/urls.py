e79521197e34820c3f5038d057752226a4882cf5
Bastonator <edwarddili092@gmail.com>
2024-01-27 16:28:59 +0000

    final touches

--- pos/urls.py
+++ pos/urls.py
@@ -21,7 +21,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls,name="admin-site"),
     path('', include('posApp.urls')),
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_URL)
