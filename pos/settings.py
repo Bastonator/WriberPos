dd609d17d421482ed02b45a9f4e3ef50b2f5443b
Bastonator <edwarddili092@gmail.com>
2024-01-27 15:46:24 +0000

    final touches

--- pos/settings.py
+++ pos/settings.py
@@ -128,12 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS =[
-    BASE_DIR / "static",
-]
-STATIC_ROOT = "/staticfiles/"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
